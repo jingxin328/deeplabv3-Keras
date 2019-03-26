8d8c1cf62deb72bda1bbebc56a08b1d81228cfe3
holy_angel <nj.daixuwen@gmail.com>
2019-03-26 19:29:20 +0800

    尝试关闭坐标轴和输出图片的白边

--- get_labels.py
+++ get_labels.py
@@ -15,8 +15,15 @@ pad_x = int(512 - resized.shape[0])
 resized2 = np.pad(resized, ((0, pad_x), (0, 0), (0, 0)), mode='constant')
 res = deeplab_model.predict(np.expand_dims(resized2, 0))
 labels = np.argmax(res.squeeze(), -1)
+print(labels.shape)
 plt.imshow(labels[:-pad_x])
-plt.savefig('imgs/result_250.jpg')
+plt.axis('off')  # 关闭坐标轴
+frame = plt.gca()
+# y 轴不可见
+frame.axes.get_yaxis().set_visible(False)
+# x 轴不可见
+frame.axes.get_xaxis().set_visible(False)
+# plt.savefig('imgs/result_250.jpg')
 plt.show()
 
 
