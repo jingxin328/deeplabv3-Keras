7c920846b574d4098df645cd7c2b54ac74d52c93
holy_angel <nj.daixuwen@gmail.com>
2019-03-31 12:34:32 +0800

    对原分辨率进行分析，但效果不是很好

--- get_labels.py
+++ get_labels.py
@@ -4,18 +4,28 @@ import numpy as np
 from model import Deeplabv3
 
 
+deeplab_model = Deeplabv3(input_shape=(376, 1241, 3), classes=4)
+img = plt.imread("imgs/110.jpg")
+print(img.shape)
+img = img / 127.5 - 1
+res = deeplab_model.predict(np.expand_dims(img, 0))
+labels = np.argmax(res.squeeze(), -1)
+plt.imshow(labels)
+plt.show()
+
+'''
 deeplab_model = Deeplabv3()
 img = plt.imread("imgs/250.jpg")
 print(img.shape)
 w, h, _ = img.shape
 ratio = 512. / np.max([w, h])
 resized = cv2.resize(img, (int(ratio*h), int(ratio*w)))
-resized = resized / 127.5 - 1.
+resized = resized / 127.5 - 1    # 将图像归一化到[-1,1]
 pad_x = int(512 - resized.shape[0])
 resized2 = np.pad(resized, ((0, pad_x), (0, 0), (0, 0)), mode='constant')
 res = deeplab_model.predict(np.expand_dims(resized2, 0))
+print(res.shape)
 labels = np.argmax(res.squeeze(), -1)
-print(labels.shape)
 plt.imshow(labels[:-pad_x])
 plt.axis('off')  # 关闭坐标轴
 frame = plt.gca()
@@ -25,5 +35,5 @@ frame.axes.get_yaxis().set_visible(False)
 frame.axes.get_xaxis().set_visible(False)
 plt.savefig('imgs/result_250.jpg', bbox_inches='tight', pad_inches=0)
 plt.show()
-
+'''
 
